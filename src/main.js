02bf57c84ed5783168384edfe8614199cd9bb290
priyanshu <patnayakpriyanshu@gmail.com>
2024-11-27 15:44:26 +0530

    fixe : 🐛  announce response fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
+import { TrackerManager } from './tracker-manager.js';
 
-import URLParse from  'url-parse';
-import * as dgram from 'dgram';
-import { TrackerManger } from './tracker-manager.js';
+const trackerManager = new TrackerManager('/home/priyanshu/Desktop/workshop/torrent-recreation/src/torrent-file/t9.torrent');
+// trackerManager.udpSendRequest(trackerManager.connectRequest() , (response) =>{console.log('my response is :' , response)});
 
-const trackerManager = new TrackerManger('/home/priyanshu/Desktop/workshop/torrent-recreation/src/torrent-file/t9.torrent');
-trackerManager.udpSendRequest(trackerManager.connectRequest() , (response) =>{console.log('my response is :' , response)})
+trackerManager.getPeers();
