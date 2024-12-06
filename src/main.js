b006553ea88bc4d7536307ebd54607ab089ff6bb
priyanshu <patnayakpriyanshu@gmail.com>
2024-12-06 12:04:28 +0530

    🎸 - feat : peers connection and pieces info

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 import {Client} from './Client.js'
-
 const client = new Client();
-
-client.addTorrent('/home/priyanshu/Desktop/workshop/torrent-recreation/src/torrent-file/t1.torrent')
+client.addTorrent('/home/priyanshu/Desktop/workshop/torrent-recreation/src/torrent-file/t1fk.torrent')
